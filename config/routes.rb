58cb1cd4e1912cb8dcba600367146ad1f140d11b
Jai Keerthi <jai@Jai-Keerthis-MacBook-Pro.local>
2012-03-11 19:10:02 +0530

    cleaned and initialized home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SocialApp::Application.routes.draw do
+  root :to => 'home#index'
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,7 +51,7 @@ SocialApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
